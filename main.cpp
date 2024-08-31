de241cd02748d9808cd6444339ce366f7e8835af
Pedro Greco <rgreco.pedro@gmail.com>
2024-08-31 15:27:06 -0300

    Started factorial list

--- main.cpp
+++ main.cpp
@@ -10,6 +10,31 @@ int addList(int arrayOfNumbers[], int arraySize) {
     return sum;
 }
 
+//Factorial of the elements in a list
+
+//numberList= list(range(1,11))
+//factorialList = []
+
+//for i in numberList:
+    //for x in range(1,i):
+    //    i*=x;
+
+  //  factorialList.append(i)
+
+//print(numberList)
+//print(factorialList)
+long listFactorial(int arrayOfNumbers[], int arraySize){
+    long factorialList[arraySize]={};
+    for(int number; number<arraySize; number++){
+        for(int smallerNumber; smallerNumber<number; smallerNumber++){
+            number*=
+        }
+    }
+
+}
+
+
+
 int main(){
     int numberList[10] = {1,2,3,4,5,6,7,8,9,10};
     int listSize = sizeof(numberList)/sizeof(numberList[0]);
